ee9af720f1d749af606f626dbf95e97f678d74be
Glauco Goncalves <glaestgon@gmail.com>
2015-03-19 11:32:45 -0700

    Novas demos

--- rsi/socketerror.py
+++ rsi/socketerror.py
@@ -26,16 +26,3 @@ class socketError(socket.socket):
                 return b"Error"
         else:
             return self.recv(n)        
-
-HOST = 'localhost'
-PORT = 12000
-s = socketError(socket.AF_INET, socket.SOCK_DGRAM)
-s.setErrorProb(0.5)
-s.connect((HOST, PORT))
-s.sendWithError("teste")
-data = s.recvWithError(1024)
-print(data)
-s.sendWithError("testando")
-data = s.recvWithError(1024)
-print(data)
-s.close()

8e86727b30774cf9a96658afc6b3d51ecaef7d26
Glauco Gonçalves <glaestgon@gmail.com>
2015-10-07 04:02:55 -0700

    Adding timeout example

--- rsi/socketerrorTest.py
+++ rsi/socketerrorTest.py
@@ -4,11 +4,21 @@ HOST = 'localhost'
 PORT = 12000
 s = socketerror.socketError(socket.AF_INET, socket.SOCK_DGRAM)
 s.setErrorProb(0.5)
+s.settimeout(2.0)
 s.connect((HOST, PORT))
 s.sendWithError("teste")
-data = s.recvWithError(1024)
-print(data)
+try:
+    data = s.recvWithError(1024)
+    print(data)
+except socket.timeout:
+    print("Timeout")
+
 s.sendWithError("testando")
-data = s.recvWithError(1024)
-print(data)
+
+try:
+    data = s.recvWithError(1024)
+    print(data)
+except socket.timeout:
+    print("Timeout")
+
 s.close()

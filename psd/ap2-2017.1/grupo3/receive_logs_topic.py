ab235bc2c18e89d8798bfcd9e911bfe2f1babc4f
mininet <victorwcm@gmail.com>
2017-06-12 07:36:32 -0700

    grupo3

--- psd/ap2-2017.1/grupo3/receive_logs_topic.py
+++ psd/ap2-2017.1/grupo3/receive_logs_topic.py
@@ -2,26 +2,43 @@
 import pika
 import sys
 
-credentials = pika.PlainCredentials('guest', 'guest')
+credentials = pika.PlainCredentials('avaliacaoparcial2', 'voutirar10')
 connection = pika.BlockingConnection(pika.ConnectionParameters(
-               'localhost', 5672, '/', credentials))
+               '172.16.205.243', 15672, 'meteorologia', credentials))
 channel = connection.channel()
 
 channel.exchange_declare(exchange='topic_logs',
                          type='topic')
 
 result = channel.queue_declare(exclusive=True)
+
 queue_name = result.method.queue
+#routing_key = sys.argv[1]
 
-binding_keys = sys.argv[1:]
-if not binding_keys:
-    print >> sys.stderr, "Usage: %s [binding_key]..." % (sys.argv[0],)
-    sys.exit(1)
 
-for binding_key in binding_keys:
-    channel.queue_bind(exchange='topic_logs',
-                       queue=queue_name,
-                       routing_key=binding_key)
+valor= body.split(",")
+tipo= method.routing_key.split(".")
+if (len(sys.argv) == 2):
+	if((valor[1] > 100) and (tipo[1] == "precipitacao")):
+		message= valor[1] + "." + valor[0];
+		routing_key= tipo[0] + ".alerta." + tipo[1]; 
+	if((valor[1] > 100) and (tipo[1] == "velocidade-vento")):
+		message= valor[1] + "." + valor[0];
+		routing_key= tipo[0] + ".alerta." + tipo[1];
+	if((valor[1] > 5) and (tipo[1] == "radiacao-uv")):
+		message= valor[1] + "." + valor[0];
+		routing_key= tipo[0] + ".alerta." + tipo[1];
+	chanel.basic_publish(exchange='topic_logs', routing_key=routing_key, body=message);
+
+#binding_keys = sys.argv[1:]
+#if not binding_keys:
+#    print >> sys.stderr, "Usage: %s [binding_key]..." % (sys.argv[0],)
+#    sys.exit(1)
+
+#for binding_key in binding_keys:
+#    channel.queue_bind(exchange='topic_logs',
+#                       queue=queue_name,
+#                       routing_key=binding_key)
 
 print ' [*] Waiting for logs. To exit press CTRL+C'
 

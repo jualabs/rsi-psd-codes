8e9a87688836ffc8c383cec0422851c8c85a68b7
Victor Medeiros <victorwcm@gmail.com>
2017-10-20 14:30:27 -0300

    Client Walk enhancements

--- psd/rmi_demo/Walk/ClientWalk.java
+++ psd/rmi_demo/Walk/ClientWalk.java
@@ -1,35 +1,51 @@
-import java.rmi.Naming;
-import java.rmi.RemoteException;
+// import java.rmi.Naming;
+// import java.rmi.RemoteException;
+import java.util.Scanner;
 
 public class ClientWalk {
 	
 	public static void main(String[] args) {
 		
-		IWalk rwalk = null;
+//		IWalk rwalk = null;
+		Scanner input = new Scanner(System.in);
 		
 		try {
-			String objname =  "rmi://localhost/walkserver";
-			System.out.println("Procurando pelo objeto " + objname);
-			rwalk = (IWalk) Naming.lookup(objname);
+//			String objname =  "rmi://localhost/walkserver";
+//			System.out.println("Procurando pelo objeto " + objname);
+//			rwalk = (IWalk) Naming.lookup(objname);
 		}
 		catch (Exception e) {
-			System.err.println("Problemas ao executar o lookup! " + e);
-			e.printStackTrace();
-			System.exit(2);
+//			System.err.println("Problemas ao executar o lookup! " + e);
+//			e.printStackTrace();
+//			System.exit(2);
 		}
-		try {
-			int n = 5;
-			String resp = null;
-			resp = rwalk.total();
-			System.out.println("Resposta recebida:\n\t" + resp);
-			rwalk.move(n);
-			resp = rwalk.total();
-			System.out.println("Resposta recebida:\n\t" + resp);
-		}
-		catch (RemoteException re) {
-			System.err.println("Problemas durante chamada remota! " + re);
-			re.printStackTrace();
-			System.exit(3);
+		while(true) {
+			try {
+			
+				System.out.print("Digite o número de passos que deseja movimentar ou 't' para total de passos já realizados ou 's' para sair: ");
+				String inputString = input.next();
+				if(inputString.equals("t")) {
+//			    		System.out.println("\n" + rwalk.total());
+					System.out.println(inputString);
+				}
+				else if(inputString.equals("s") {
+			    		System.exit(0);
+				}
+				else {
+					Integer.parseInt(inputString);
+//					rwalk.move(inputSring);
+					system.out.println(inputString);				
+				}
+			}
+//			catch (RemoteException re) {
+//				System.err.println("Problemas durante chamada remota! " + re);
+//				re.printStackTrace();
+//				System.exit(3);
+//			}
+			catch (NumberFormatEception nfe) {
+				System.out.println("Digite um valor válido!");
+				continue;	
+			}
 		}
 	}
 }
--- psd/rmi_demo/Walk/Walk.java
+++ psd/rmi_demo/Walk/Walk.java
@@ -2,19 +2,21 @@ import java.rmi.server.UnicastRemoteObject;
 import java.rmi.RemoteException;
 
 public class Walk extends UnicastRemoteObject implements IWalk {
+	
 	private static final long serialVersionUID = 1L;
+	
 	int nsteps = 0;
 	
 	public Walk() throws RemoteException {
 	}
 	
 	public void move(int n) {
-		System.out.println("mais um movimento ("+n+" passos)!");
+		System.out.println("registrado movimento de " + n + " passos!");
 		nsteps += n;
 	}
 	
 	public String total() {
-		String returnString = "jah andou um total de " + nsteps + " passos!\n";
+		String returnString = "total de " + nsteps + " passos já realizados!\n";
 		return(returnString);
 	}
 }

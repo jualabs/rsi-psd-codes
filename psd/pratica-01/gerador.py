ebcc3ba1c8d09da5b7e0658f330049c54b0a1e7a
victormedeiros <victorwcm@gmail.com>
2019-04-07 14:48:47 -0300

    correcao dicionario para array

--- psd/pratica-01/gerador.py
+++ psd/pratica-01/gerador.py
@@ -19,26 +19,26 @@ channel = connection.channel()
 channel.exchange_declare(exchange='topic_logs',
                          exchange_type='topic')
 
-messages = {'petrolina.radiacao-uv':4,          'petrolina.radiacao-uv':6,
-            'petrolina.velocidade-vento':120,   'petrolina.velocidade-vento':80,
-	    'petrolina.precipitacao':70,        'petrolina.precipitacao':90,
-            'parnamirim.radiacao-uv':3,         'parnamirim.radiacao-uv':2,
-            'parnamirim.velocidade-vento':60,   'parnamirim.velocidade-vento':130,
-            'parnamirim.precipitacao':110,      'parnamirim.precipitacao':90,
-            'serra-talhada.radiacao-uv':7,      'serra-talhada.radiacao-uv':3,
-            'serra-talhada.velocidade-vento':90,'serra-talhada.velocidade-vento':80,
-            'serra-talhada.precipitacao':120,   'serra-talhada.precipitacao':90,
-            'pesqueira.radiacao-uv':4,          'pesqueira.radiacao-uv':3,
-            'pesqueira.velocidade-vento':160,   'pesqueira.velocidade-vento':80,
-            'pesqueira.precipitacao':90,        'pesqueira.precipitacao':70,
-            'recife.radiacao-uv':3,             'recife.radiacao-uv':4,
-            'recife.velocidade-vento':70,       'recife.velocidade-vento':70,
-            'recife.precipitacao':120,          'recife.precipitacao':80}
+messages = [['petrolina.radiacao-uv',4],          ['petrolina.radiacao-uv',6],
+            ['petrolina.velocidade-vento',120],   ['petrolina.velocidade-vento',80],
+	    ['petrolina.precipitacao',70],        ['petrolina.precipitacao',90],
+            ['parnamirim.radiacao-uv',3],         ['parnamirim.radiacao-uv',2],
+            ['parnamirim.velocidade-vento',60],   ['parnamirim.velocidade-vento',130],
+            ['parnamirim.precipitacao',110],      ['parnamirim.precipitacao',90],
+            ['serra-talhada.radiacao-uv',7],      ['serra-talhada.radiacao-uv',3],
+            ['serra-talhada.velocidade-vento',90],['serra-talhada.velocidade-vento',80],
+            ['serra-talhada.precipitacao',120],   ['serra-talhada.precipitacao',90],
+            ['pesqueira.radiacao-uv',4],          ['pesqueira.radiacao-uv',3],
+            ['pesqueira.velocidade-vento',160],   ['pesqueira.velocidade-vento',80],
+            ['pesqueira.precipitacao',90],        ['pesqueira.precipitacao',70],
+            ['recife.radiacao-uv',3],             ['recife.radiacao-uv',4],
+            ['recife.velocidade-vento',70],       ['recife.velocidade-vento',70],
+            ['recife.precipitacao',120],          ['recife.precipitacao',80]]
 
-for k,v in messages.iteritems():
-	message = '%d,%d' % (time.time(), v)
-	channel.basic_publish(exchange='topic_logs', routing_key=k, body=message)
-	print " [x] Sent %r:%r" % (k, message)
+for i in messages:
+	message = '%d,%d' % (time.time(), i[1])
+	channel.basic_publish(exchange='topic_logs', routing_key=i[0], body=message)
+	print " [x] Sent %r:%r" % (i[0], message)
 	time.sleep(2)
 
 connection.close()
